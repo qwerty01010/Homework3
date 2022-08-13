5d8d7a90ffbdb6d36f19663bb95a64477b0340e0
qwerty01010 <110329988+qwerty01010@users.noreply.github.com>
2022-08-13 12:56:18 +0300

    HW2&3

---
+++ HW3_2.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine ("Вычисление расстояния между точками");
+
+Console.WriteLine ("Введите координату х первой точки:");
+int x1 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите координату y первой точки:");
+int y1 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите координату z первой точки:");
+int z1 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите координату x второй точки:");
+int x2 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите координату y второй точки:");
+int y2 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Введите координату z второй точки:");
+int z2 = int.Parse(Console.ReadLine ()!);
+
+Console.WriteLine ("Координаты первой точки: " + x1 + ", "+ y1 + ", "+ z1 + "; Координаты второй точки: " + x2 + ", " + y2 + ", "+ z2);
+
+double result = Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) + ((z2-z1)*(z2-z1)));
+
+Console.WriteLine("\n");
+Console.WriteLine ("Расстояние между точками х и у = " + result.ToString("N2"));
---
+++ HW3_3.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine ("Таблица кубов");
+
+Console.WriteLine ("Введите число");
+int x = int.Parse(Console.ReadLine ()!);
+
+int i = 1;
+while (x >= i) 
+{
+    var result = Math.Pow (i, 3);
+    i++;
+    Console.WriteLine (result);
+}
